1e3f887d7336ab8bf4e04be64183bfb9f9f83397
maciejwieczorek <36855045+maciejwieczorek@users.noreply.github.com>
2018-03-14 13:12:52 +0100

    Add files via upload

---
+++ lekcja_1/zad_2.py
@@ -0,0 +1,19 @@
+print('Wprowadz liczbe :')
+a = int(input())
+b = int(input())
+c = int(input())
+
+if ((a > b ) & ( a > c)):
+    print(a)
+elif ( (b > a) & (b > c) ):
+    print(b)
+else:
+    print(c)
+
+
+
+
+
+
+
+
