1e99041c600675e69950cf610e186792882f3819
maciejwieczorek <36855045+maciejwieczorek@users.noreply.github.com>
2018-04-25 13:51:31 +0200

    Add files via upload

--- lekcja_8/zad3.py
+++ lekcja_8/zad3.py
@@ -3,28 +3,28 @@
 import subprocess
 import os
 
+import operator
+import functools as fun
 
-from math import *
- 
-def rozklad(x):
-    if x<=0:
-        return 0
-    i=2
-    e=floor(sqrt(x))
-    r=[] #używana jest tablica (lista), nie bepośrednie wypisywanie
-    while i<=e:
-        if x%i==0:
-            r.append(i)
-            x/=i
-            e=floor(sqrt(x))
-        else:
-            i+=1
-    if x>1: r.append(x)
-        return r
- 
+def primeFactorization(num): 
+    num = num
+    primes = []
+    result = []
+    if num > 1:
+        [primes.append(x) for x in range(2, num) if all(x % y != 0 for y in range(2, x))]
+    multy = fun.reduce(operator.mul, result, 1)
+    for number in primes:
+        if num % number == 0:
+            result.append(number)
+
+    multy = fun.reduce(operator.mul, result, 1)
+    y = num/multy
+    if y != 1 and y in primes:
+        result.insert(0, int(y))
+    return result
+    
+print(primeFactorization(756))
  
 
-rozklad(756)
-print r
     
     
